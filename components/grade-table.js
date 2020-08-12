1ae368950388833574ef570615b7e1fa09604929
daniejlee <daniejlee@gmail.com>
2020-08-11 18:56:29 -0700

    render grades 9

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,6 +3,22 @@ class GradeTable {
     this.tableElement = tableElement;
   }
   updateGrades(grades){
-    console.log(grades)
+    var tbody = this.tableElement.querySelector('tbody');
+    var existingRows = tbody.querySelectorAll('tr')
+    for (var i = 0; i < existingRows.length; i++){
+      existingRows[i].remove();
+    }
+    for(var j = 0; j < grades.length; j++){
+      var newRow = document.createElement('tr');
+      var name = document.createElement('td');
+      var course = document.createElement('td');
+      var grade = document.createElement('td');
+
+      name.textContent = grades[j].name;
+      course.textContent = grades[j].course;
+      grade.textContent = grades[j].grade;
+      newRow.append(name, course, grade)
+      tbody.appendChild(newRow);
+    }
   }
 }
